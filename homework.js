4787c01e85e9a0be4c16d86d184fb4097c19b3ea
hollyhbrady <hollyhbrady@gmail.com>
2021-05-03 17:59:57 +0100

    Q2, 3, 4 done

--- homework.js
+++ homework.js
@@ -1,33 +1,65 @@
 // 1 `The murderer is Miss Scarlet.`
 // RIGHT
 
-const scenario = {
-    murderer: 'Miss Scarlet',
-    room: 'Library',
-    weapon: 'Rope'
-  };
+// const scenario = {
+//     murderer: 'Miss Scarlet',
+//     room: 'Library',
+//     weapon: 'Rope'
+//   };
   
-  const declareMurderer = function() {
-    return `The murderer is ${scenario.murderer}.`;
-  }
+//   const declareMurderer = function() {
+//     return `The murderer is ${scenario.murderer}.`;
+//   }
   
-  const verdict = declareMurderer();
-  console.log(verdict);
+//   const verdict = declareMurderer();
+//   console.log(verdict);
 
 // 2 The murderer is Mrs. Peacock.
-// WRONG - SyntaxError: Identifier 'declareMurderer' has already been declared.
-// I don't understand, I though you could 
+// WRONG - TypeError: Assignment to constant variable line 24
+// I don't understand, I though you could reassign a const through a function?
 
-const murderer = 'Professor Plum';
+// const murderer = 'Professor Plum';
 
-const changeMurderer = function() {
-  murderer = 'Mrs. Peacock';
+// const changeMurderer = function() {
+//   murderer = 'Mrs. Peacock';
+// }
+
+// const declareMurderer = function() {
+//   return `The murderer is ${murderer}.`;
+// }
+
+// changeMurderer();
+// const verdict = declareMurderer();
+// console.log(verdict);
+
+// 3 The murderer is Mrs. Peacock. and an error about the second verdict syntax
+// 1/2 First Verdict:  The murderer is Mrs. Peacock. Second Verdict:  The murderer is Professor Plum.
+// Does this work because verdict 1 calls the function and verdict 2 calls just murderer?
+// let murderer = 'Professor Plum';
+
+// const declareMurderer = function() {
+//   let murderer = 'Mrs. Peacock';
+//   return `The murderer is ${murderer}.`;
+// }
+
+// const firstVerdict = declareMurderer();
+// console.log('First Verdict: ', firstVerdict);
+
+// const secondVerdict = `The murderer is ${murderer}.`;
+// console.log('Second Verdict: ', secondVerdict);
+
+// 4 The suspects are Miss Scarlet, Professor Plum, Colonel Mustard. Suspect three is Mrs. Peacock.
+// RIGHT
+
+let suspectOne = 'Miss Scarlet';
+let suspectTwo = 'Professor Plum';
+let suspectThree = 'Mrs. Peacock';
+
+const declareAllSuspects = function() {
+  let suspectThree = 'Colonel Mustard';
+  return `The suspects are ${suspectOne}, ${suspectTwo}, ${suspectThree}.`;
 }
 
-const declareMurderer = function() {
-  return `The murderer is ${murderer}.`;
-}
-
-changeMurderer();
-const verdict = declareMurderer();
-console.log(verdict);
+const suspects = declareAllSuspects();
+console.log(suspects);
+console.log(`Suspect three is ${suspectThree}.`);
